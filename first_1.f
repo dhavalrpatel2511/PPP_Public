1c83972837639f0153af1db865f0489123f13533
Dhaval Rasheshk Patelumar <dp44dafy@imfd053.imfd.tu-freiberg.de>
2019-12-09 17:48:26 +0100

    add common parameters and dimensions of standard variables.

--- first_1.f
+++ first_1.f
@@ -14,3 +14,19 @@ c
      1 energy(*),coords(mcrd,nnode),u(ndofel),du(mlvarx,*),v(ndofel),
      2 a(ndofel),time(2),params(*),jdltyp(mdload,*),adlmag(mdload,*),
      3 ddlmag(mdload,*),predef(2,npredf,nnode),lflags(*),jprops(*)
+c
+      parameter (ndim=2, ndof=5, ndi=3, nshr=1, nnodemax=8,
+     1     ntens=4, ninpt=9, nsvint=13)
+c     
+c      ndim  ... number of spatial dimensions
+c      ndof  ... number of degrees of freedom per node
+c      ndi   ... number of direct stress components
+c      nshr  ... number of shear stress component
+c      ntens ... total number of stress tensor components (=ndi+nshr)
+c      ninpt ... number of integration points
+c      nsvint... number of state variables per integration pt 
+c     
+      dimension stiff(ndof*nnodemax,ndof*nnodemax),force(ndof*nnodemax),
+     1 dN(nnodemax),dshape(ndim,nnodemax),xjaci(ndim,ndim),
+     2 bmat(nnodemax*ndim),statevLocal(nsvint),stress(ntens), 
+     3 ddsdde(ntens,ntens),stran(ntens),dstran(ntens),wght(ninpt)  
