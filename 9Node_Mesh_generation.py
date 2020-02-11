1dfc096affaf9cd819b3b2d6a5bfd87a2b904340
Dhaval Rasheshk Patelumar <dp44dafy@imfd053.imfd.tu-freiberg.de>
2020-02-11 17:18:12 +0100

    Update the mesh_generation_file.

--- 9Node_Mesh_generation.py
+++ 9Node_Mesh_generation.py
@@ -4,29 +4,31 @@ import math as math
 import csv
 from copy import deepcopy
 
-# load the nodes and element data
+# load the nodes and element data of 8-noded element.
+
 node_file = 'nodes.dat'
 element_file = 'elements.dat'
 nodes = np.loadtxt(node_file,delimiter=",")
 elements = np.loadtxt(element_file,delimiter=",")
 
-# find the shape of nodes and elements matrix
-#print(nodes)
+# find the shape of nodes and elements matrix.
+
 nodes_shape = np.shape(nodes)
 elements_shape = np.shape(elements)
 a1 = nodes_shape[0]
 a2 = nodes_shape[1]
 b1 = elements_shape[0]
 b2 = elements_shape[1]
-#print(a1,a2,b1,b2)
 
-# initialize the shape function array
+# initialize the shape function array and assign the values to the variables.
+
 shape_function = np.zeros(8)
 xi = 0
 omega = 0
-#print(shape_function)
 
-# Assign the values to the shape function array
+
+# Assign the values to the shape function array.
+
 shape_function[0] = (1/4)*(1-xi)*(1-omega)*(-xi-omega-1)
 shape_function[1] = (1/4)*(1+xi)*(1-omega)*(+xi-omega-1)
 shape_function[2] = (1/4)*(1+xi)*(1+omega)*(+xi+omega-1)
@@ -37,27 +39,32 @@ shape_function[6] = (1/2)*(1-xi**2)*(1+omega)
 shape_function[7] = (1/2)*(1-xi)*(1-omega**2)
 
 # initilize the new nodes and elements matrix.
+
 New_nodes = np.zeros((a1+b1,a2))
 New_elements = np.zeros((b1,b2+1))
 
-# assign the same nodes matrix values to the new node matrix
+# assign the same nodes matrix values to the new node matrix.
+
 for i in range(a1):
     for j in range(a2):
         New_nodes[i,j] = nodes[i,j]
 
-# assign the same element matrix values to the new element matrix
+# assign the same element matrix values to the new element matrix.
+
 for i in range(b1):
     for j in range(b2):
         New_elements[i,j] = elements[i,j]      
 
-# loop to assign the addition nodes to the new nodes matrix
-# loop to add the nodes number to the new element matrix
+# loop to assign the addition nodes to the new nodes matrix.
+# loop to add the nodes number to the new element matrix.
+
 for i in range(a1,a1+b1,1):
     New_nodes[i,:] = 0
     New_nodes[i,0] = i+1
     element_number = i - a1 + 1
 
-# loop to find coordinate of the new ninth node of the element 
+# loop to find coordinate of the new ninth node of the element. 
+
     for j in range(1,8+1,1):
         node_number = elements[element_number-1,j]
         node_number = np.int(node_number)
@@ -65,39 +72,17 @@ for i in range(a1,a1+b1,1):
         New_nodes[i,2] = New_nodes[i,2] + shape_function[j-1]*nodes[node_number-1,2]
     New_elements[element_number-1,9] = i+1
     
-# New_nodes_column_one = np.zeros_like(New_nodes[:,0])
-# print(New_nodes[:,0]) 
-# New_nodes_column_one = New_nodes[:,0]
-# New_nodes_column_one = New_nodes_column_one.astype(int)
-# print(New_nodes_column_one)
-# New_nodes[:,0] = New_nodes_column_one[:]
-# New_nodes_1 = np.zeros_like(New_nodes[:,1])
-# New_nodes_2 = np.zeros_like(New_nodes[:,2])
-# New_nodes_1 = New_nodes[:,1] 
-# New_nodes_2 = New_nodes[:,2]
-# print(New_nodes)
-# New_nodes = New_nodes#.astype(int)
-# new_row = np.array([int(x) for x in New_nodes[:, 0]])
-# New_nodes[:, 0] = np.around(new_row, decimals=0)
-# print('hi', new_row)
-# print(New_nodes_1)
-# print(New_nodes_2)
-# New_nodes[:,1] = New_nodes_1[:]
-# New_nodes[:,2] = New_nodes_2[:] 
-# print(New_nodes) 
-# print(New_nodes)
-# with open('New_nodes.dat', 'w', newline='') as file:
-# writer = csv.writer(file)
-# writer.writerows(New_nodes)
+# convert new element matrix into integer matrix from folat matrix.
 
-# convert new element matrix into integer matrix from folat matrix
 New_elements = New_elements.astype(int)  
 with open('New_nodes.dat', 'w') as file:
     for i in range(New_nodes.shape[0]):
 
-# write the matrix column vice to convert the first column of new node matrix
+# write the matrix column vice to convert the first column of new node matrix.
 # into the integer since it contains the node number.
+
         file.write(str(int(New_nodes[i, 0])) + ',' + str(New_nodes[i, 1]) + ',' + str(New_nodes[i, 2]) + '\n')
+        
 with open('New_elements.dat', 'w', newline='') as file1:
     writer = csv.writer(file1)
     writer.writerows(New_elements)    
